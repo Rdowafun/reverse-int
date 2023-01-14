bbeaf4b1fa12fc200c46f3bd578f8d76da433f45
Rdowafun <jabrooster@gmail.com>
2023-01-14 17:57:07 +0500

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = ""
+    n = n < 0 ? n * -1 : n
+    n += ""
+    for (let i = n.length - 1; i >= 0; i--) {
+        str = str + n[i]
+    }
+    return str
 }
